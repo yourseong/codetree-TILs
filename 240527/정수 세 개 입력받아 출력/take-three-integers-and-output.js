8e942b32ddcdba63c41b867c0b9b4bef6133737e
Kang soyeong <korea020808@naver.com>
2024-05-27 17:02:16 +0900

    Title: 정수 세 개 입력받아 출력, Time: 224ms, Memory: 14MB, Status: Passed - CodeTree

--- 240527/정수 세 개 입력받아 출력/take-three-integers-and-output.js
+++ 240527/정수 세 개 입력받아 출력/take-three-integers-and-output.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 
 let num = fs.readFileSync(0).toString().split((/,| |\n/));
 
+let a = num[0]
+let b = num[1]
+let c = num[2]
 
-
-console.log(num[0], [1], [2]);
+console.log(a, b, c);
