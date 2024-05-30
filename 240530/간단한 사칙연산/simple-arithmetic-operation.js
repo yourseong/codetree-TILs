3ae86599ff703633a427a64caa75b2a959d069af
Kang soyeong <korea020808@naver.com>
2024-05-30 15:30:48 +0900

    Title: 간단한 사칙연산, Time: 223ms, Memory: 14MB, Status: Passed - CodeTree

--- 240530/간단한 사칙연산/simple-arithmetic-operation.js
+++ 240530/간단한 사칙연산/simple-arithmetic-operation.js
@@ -1,8 +1,12 @@
+//두 자릿수를 입력받는 경우도 생각해야함
+
 const fs = require("fs");
-let input = fs.readFileSync(0).toString().split("");
+// let input = fs.readFileSync(0).toString().split('');
+
+let input = fs.readFileSync(0).toString().split(/\s+/);
 
 let a = Number(input[0]);
-let b = Number(input[2]);
+let b = Number(input[1]);
 
 let plus = a + b;
 let minus = a - b;
