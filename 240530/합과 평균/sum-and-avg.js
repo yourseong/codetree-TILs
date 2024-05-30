60ce09bb3d3408528e33a00d71611d9395d6fa9b
Kang soyeong <korea020808@naver.com>
2024-05-30 16:25:43 +0900

    Title: 합과 평균, Time: 181ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240530/합과 평균/sum-and-avg.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().split(/\s+/);
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+let sum = a + b;
+let average = (a + b) / 2;
+
+console.log(sum, average);
