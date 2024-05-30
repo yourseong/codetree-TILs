d87d2fc85d756f100ad85d3450d52412dbf1f410
Kang soyeong <korea020808@naver.com>
2024-05-30 15:47:59 +0900

    Title: 두수의 곱과 몫, Time: 179ms, Memory: 14MB, Status: Passed - CodeTree

--- 240530/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
+++ 240530/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
@@ -5,7 +5,7 @@ let num1 = Number(input[0]);
 let num2 = Number(input[1]);
 
 let multiply = num1 * num2;
-let divide = num1 / num2;
+let divide = parseInt(num1 / num2);
 
 console.log(`${num1} * ${num2} = ${multiply}`);
 console.log(`${num1} / ${num2} = ${divide}`);
