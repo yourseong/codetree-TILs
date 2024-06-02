bda6bb90806ab1a813a7d8889f7a4d836e65a004
Kang soyeong <korea020808@naver.com>
2024-06-02 22:16:34 +0900

    Title: 2개 중 최대, Time: 262ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240602/2개 중 최대/max-of-two-nums.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+let c = a > b ? a : b;
+
+console.log(c);
