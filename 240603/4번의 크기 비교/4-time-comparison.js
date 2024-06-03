77f764c75d11534164ce08866f3eda170f9cb42f
Kang soyeong <korea020808@naver.com>
2024-06-03 17:04:45 +0900

    Title: 4번의 크기 비교, Time: 175ms, Memory: 47MB, Status: Passed - CodeTree

--- 240603/4번의 크기 비교/4-time-comparison.js
+++ 240603/4번의 크기 비교/4-time-comparison.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split("\n");
-let line2 = input[1];
+let line2 = input[1]; //여기서 바로 split 사용해도 됨
 let bcde = line2.split(" ");
 
 let a = Number(input[0]);
@@ -8,7 +8,6 @@ let b = Number(bcde[0]);
 let c = Number(bcde[1]);
 let d = Number(bcde[2]);
 let e = Number(bcde[3]);
-
 
 console.log(a >= b ? 1 : 0);
 console.log(a > c ? 1 : 0);
