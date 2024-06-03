4cf39cd160bc29f647d679e0d37f28b6453adc7f
Kang soyeong <korea020808@naver.com>
2024-06-03 14:26:02 +0900

    Title: 살 수 있는 물건 2, Time: 182ms, Memory: 14MB, Status: Passed - CodeTree

--- 240603/살 수 있는 물건 2/things-able-to-buy-2.js
+++ 240603/살 수 있는 물건 2/things-able-to-buy-2.js
@@ -5,9 +5,9 @@ let n = Number(input);
 
 if (n >= 3000) {
     console.log("book");
-} else if (n == 1000) {
+} else if (n >= 1000) {
     console.log("mask");
-} else if (n == 500) {
+} else if (n >= 500) {
     console.log("pen");
 } else {
     console.log("no");
