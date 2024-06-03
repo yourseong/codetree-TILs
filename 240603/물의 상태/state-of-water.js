aff2ccdb1b8da40de5560401a8e2b7520e336809
Kang soyeong <korea020808@naver.com>
2024-06-03 14:08:55 +0900

    Title: 물의 상태, Time: 177ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240603/물의 상태/state-of-water.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString();
+
+let temp = Number(input);
+
+if (temp <= 0) {
+    console.log("ice");
+} else if (temp >= 100) {
+    console.log("vapor");
+} else {
+    console.log("water");
+}
