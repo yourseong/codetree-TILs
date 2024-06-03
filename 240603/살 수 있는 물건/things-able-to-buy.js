67a3ead24861cee8e9f4089ed892e880cf1b4ab7
Kang soyeong <korea020808@naver.com>
2024-06-03 14:13:18 +0900

    Title: 살 수 있는 물건, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240603/살 수 있는 물건/things-able-to-buy.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString();
+
+let money = Number(input);
+
+if (money >= 3000) {
+    console.log("book");
+} else if (n >= 1000) {
+    console.log("mask");
+} else {
+    console.log("no");
+}
