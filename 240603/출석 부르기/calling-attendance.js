5842c6daf8e58077bf5cc9fec94f6080de8814f3
Kang soyeong <korea020808@naver.com>
2024-06-03 14:15:30 +0900

    Title: 출석 부르기, Time: 198ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240603/출석 부르기/calling-attendance.js
+++ 240603/출석 부르기/calling-attendance.js
@@ -4,7 +4,7 @@ let input = fs.readFileSync(0).toString();
 let n = Number(input);
 
 if (n == 1) {
-    console.log("Jhon");
+    console.log("John");
 } else if (n == 2) {
     console.log("Tom");
 } else {
