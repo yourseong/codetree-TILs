88ca7d94addc4ede825f768d5f805e2a07f15eaa
Kang soyeong <korea020808@naver.com>
2024-06-03 14:25:35 +0900

    Title: 살 수 있는 물건 2, Time: 213ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240603/살 수 있는 물건 2/things-able-to-buy-2.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim();
+
+let n = Number(input);
+
+if (n >= 3000) {
+    console.log("book");
+} else if (n == 1000) {
+    console.log("mask");
+} else if (n == 500) {
+    console.log("pen");
+} else {
+    console.log("no");
+}
