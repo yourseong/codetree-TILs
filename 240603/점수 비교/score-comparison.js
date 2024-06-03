5f730958ff567bca73741fa5f4784c908df4c9a2
Kang soyeong <korea020808@naver.com>
2024-06-03 17:12:08 +0900

    Title: 점수 비교, Time: 194ms, Memory: 16MB, Status: Passed - CodeTree

--- 240603/점수 비교/score-comparison.js
+++ 240603/점수 비교/score-comparison.js
@@ -5,11 +5,11 @@ let line1 = input[0].split(" "); //A의 수학과 영어점수
 let line2 = input[1].split(" "); //B의 수학과 영어점수
 
 let A_math = Number(line1[0]);
-let A_eng = Number(line1[0]);
+let A_eng = Number(line1[1]);
 let B_math = Number(line2[0]);
 let B_eng = Number(line2[1]);
 
-if (A_math >= B_math && A_eng >= B_eng) {
+if (A_math > B_math && A_eng > B_eng) {
     console.log(1); 
 } else {
     console.log(0);
