35492fc1343ba23c45f4a37980b4089f3e52d965
Kang soyeong <korea020808@naver.com>
2024-09-10 13:13:54 +0900

    Title: 장학금, Time: 179ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240910/장학금/scholarship.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let score = fs.readFileSync(0).toString().trim().split(" ");
+
+let midterm = Number(score[0]);
+let final = Number(score[1]);
+
+if (midterm >= 90) {
+    if (final >= 95) {
+        console.log(100000)
+    } else if (final > 90) {
+        console.log(50000)
+    } 
+} else {
+    console.log(0)
+}
