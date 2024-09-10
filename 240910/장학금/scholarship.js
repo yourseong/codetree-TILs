7944d19fa8dcbe2cf4947c91acf77be26149e370
Kang soyeong <korea020808@naver.com>
2024-09-10 13:15:56 +0900

    Title: 장학금, Time: 189ms, Memory: 14MB, Status: Passed - CodeTree

--- 240910/장학금/scholarship.js
+++ 240910/장학금/scholarship.js
@@ -4,7 +4,7 @@ let score = fs.readFileSync(0).toString().trim().split(" ");
 let midterm = Number(score[0]);
 let final = Number(score[1]);
 
-if (midterm >= 90) {
+if (midterm >= 90 && final >= 90) {
     if (final >= 95) {
         console.log(100000)
     } else if (final >= 90) {
