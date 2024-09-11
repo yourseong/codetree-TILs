c820c2b6e51179740c52b73df204aac37c51e3b0
Kang soyeong <korea020808@naver.com>
2024-09-11 09:30:02 +0900

    Title: 두 사람, Time: 163ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240911/두 사람/two-person.js
+++ 240911/두 사람/two-person.js
@@ -9,7 +9,7 @@ const p2 = input[1].split(" ");
 const p2_age = Number(p2[1]);
 const p2_sex = p2[1];
 
-if ((p1_age >= 19 && p1_sex === "M") || (p1_age >= 19 && p2_sex === "M")) {
+if ((p1_age >= 19 && p1_sex === "M") || (p2_age >= 19 && p2_sex === "M")) {
     console.log(1);
 } else {
     console.log(0);
