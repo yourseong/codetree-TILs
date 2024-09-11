118439abe701d79de8f3b676f1b8633719912d02
Kang soyeong <korea020808@naver.com>
2024-09-11 09:33:35 +0900

    Title: 두 사람, Time: 170ms, Memory: 14MB, Status: Passed - CodeTree

--- 240911/두 사람/two-person.js
+++ 240911/두 사람/two-person.js
@@ -6,7 +6,7 @@ const p1_age = Number(p1[0]);
 const p1_sex = p1[1];
 
 const p2 = input[1].split(" ");
-const p2_age = Number(p2[1]);
+const p2_age = Number(p2[0]);
 const p2_sex = p2[1];
 
 if ((p1_age >= 19 && p1_sex === "M") || (p2_age >= 19 && p2_sex === "M")) {
