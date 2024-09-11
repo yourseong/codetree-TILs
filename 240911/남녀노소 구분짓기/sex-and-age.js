f303937e404d5d60a768815b05b84d667a788244
Kang soyeong <korea020808@naver.com>
2024-09-11 09:46:18 +0900

    Title: 남녀노소 구분짓기, Time: 171ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240911/남녀노소 구분짓기/sex-and-age.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const gender = Number(input[0]);
+const age = Number(input[1]);
+
+// 남자 0, 여자 1, 19세 이상은 성인
+// 성인남자 MAN, 성인여자 WOMAN, 소년 BOY, 소녀 GIRL
+
+if (gender === 0) {
+    if (age >= 19) {
+        console.log("MAN");
+    } else {
+        console.log("BOY");
+    }
+} else {
+    if (age >= 19){
+        console.log("WONAM")
+    } else {
+        console.log("GIRL");
+    }
+}
