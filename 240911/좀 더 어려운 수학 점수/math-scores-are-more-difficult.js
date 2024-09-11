9e77faffa0ee24d6765d1bf79b2d9277855db2e5
Kang soyeong <korea020808@naver.com>
2024-09-11 09:18:02 +0900

    Title: 좀 더 어려운 수학 점수, Time: 184ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240911/좀 더 어려운 수학 점수/math-scores-are-more-difficult.js
+++ 240911/좀 더 어려운 수학 점수/math-scores-are-more-difficult.js
@@ -6,14 +6,16 @@ let A_grade = input[0].split(" ");
 let A_Math = Number(A_grade[0]);
 let A_English = Number(A_grade[1]);
 
-let B_grade = Number(input[0].split(" "));
+let B_grade = Number(input[1].split(" "));
 let B_Math = Number(B_grade[0]);
 let B_English = Number(B_grade[1]);
 
 // 영어 점수와 상관없이 수학 점수가 높다면, 더 높은 학생의 이름 출력
+// 수학 점수가 같다면 영어 점수가 더 높은 학생의 이름 출력
+
 if (A_Math > B_Math) {
     console.log("A");
-} else if (A_Math === B_Math) { // 수학 점수가 같다면 영어 점수가 더 높은 학생의 이름 출력
+} else if (A_Math === B_Math) { 
     if (A_English > B_English) {
         console.log("A");
     } else {
