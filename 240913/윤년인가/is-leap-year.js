0b496ee8a7f4b7ece73a89dd94fe70378000704c
soyeong <korea020808@naver.com>
2024-09-13 11:23:24 +0900

    Title: 윤년인가, Time: 204ms, Memory: 14MB, Status: Passed - CodeTree

--- 240913/윤년인가/is-leap-year.js
+++ 240913/윤년인가/is-leap-year.js
@@ -2,10 +2,8 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim();
 const y = Number(input);
 
-if (y % 4 === 0) {
-    console.log("true");
-} else if (y % 100 === 0 && y % 400 != 0) {
-    console.log("false")
-} else {
+if ((y % 4 !== 0) || (y % 100 === 0 && y % 400 !== 0) ) {
     console.log("false");
+} else {
+    console.log("true");
 }
