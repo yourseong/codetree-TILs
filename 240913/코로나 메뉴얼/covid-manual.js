c6a3a4d3662d24681b6283c0b7ec38ef56f04b28
soyeong <korea020808@naver.com>
2024-09-13 12:41:42 +0900

    Title: 코로나 메뉴얼, Time: 195ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240913/코로나 메뉴얼/covid-manual.js
+++ 240913/코로나 메뉴얼/covid-manual.js
@@ -28,7 +28,7 @@ if (p1_cold === "Y" || p2_cold === "Y" || p3_cold || "Y") // 증상이 있을 �
     } 
     else // 증상이 없을 때
     {
-        if (p1_temp >= 37 || p2_temp >= 37 || p3_temp >= 37 ) //증상은 없지만 37도 이상일 때
+        if ((p1_temp >= 37 && p2_temp >= 37) || (p1_temp >= 37 && p3_temp >= 37) || (p2_temp >= 37 && p3_temp >= 37)) //증상은 없지만 37도 이상일 때
        { console.log("N");}
        else {
         console.log("N");
