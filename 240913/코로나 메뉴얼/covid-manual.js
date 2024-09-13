d7e25d0def0feae075ddd518b1893bda46d46b63
soyeong <korea020808@naver.com>
2024-09-13 12:32:57 +0900

    Title: 코로나 메뉴얼, Time: 170ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240913/코로나 메뉴얼/covid-manual.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim();
+
+const person = input.split("\n");
+const p1 = person[0].split(" ");
+const p2 = person[1].split(" ");
+const p3 = person[2].split(" ");
+
+const p1_cold = p1[0];
+const p1_temp = Number(p1[1]);
+
+const p2_cold = p2[0];
+const p2_temp = Number(p2[1]);
+
+const p3_cold = p3[0];
+const p3_temp = Number(p3[1]);
+
+if (p1_cold === "Y" || p2_cold === "Y" || p3_cold || "Y") // 증상이 있을 때
+    { 
+        if ((p1_temp >= 37 && p2_temp >= 37) || (p1_temp >= 37 && p3_temp >= 37) || (p2_temp >= 37 && p3_temp >= 37)) //위급상황일때 (A로 가는 사람이 2명 이상인 경우)
+        {
+            console.log("E");
+        }
+       
+    } 
+    else//증상이 없을 때 
+    {
+        console.log("N");
+    }
