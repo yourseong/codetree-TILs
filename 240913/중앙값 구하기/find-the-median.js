f8a9c781e49fb82bf4f543f50e9b645d67ce2023
soyeong <korea020808@naver.com>
2024-09-13 13:45:02 +0900

    Title: 중앙값 구하기, Time: 316ms, Memory: 14MB, Status: Passed - CodeTree

--- 240913/중앙값 구하기/find-the-median.js
+++ 240913/중앙값 구하기/find-the-median.js
@@ -5,11 +5,10 @@ const a = Number(input[0]);
 const b = Number(input[1]);
 const c = Number(input[2]);
 
-
-if (a > b && a < c) {
+if ((a > b && a < c) || (a > c && a < b)) {
     console.log(a);
-} else if (b > a && b < c) {
+} else if ((b > a && b < c) || (b > c && a > b)) {
     console.log(b);
-} else if (c > a && c < b){
+} else if ((c > a && c < b) || (a > c && c > b)){
     console.log(c);
 }
