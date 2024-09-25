80c3ef1b430fa1781f17ce0ff8d4bb7147b83e63
soyeong <korea020808@naver.com>
2024-09-25 12:45:08 +0900

    Title: 숫자의 증감, Time: 210ms, Memory: 47MB, Status: Wrong Answer - CodeTree

---
+++ 240925/숫자의 증감/increasing-and-decreasing-numbers.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split(" ");
+
+const c = input[0];
+const n = Number(input[1]);
+
+let result = "";
+
+if (c === 'A') {
+    for (let i = 1; i <= n; i++) {
+        result += (i + " ");
+    }
+}
+
+console.log(result);
