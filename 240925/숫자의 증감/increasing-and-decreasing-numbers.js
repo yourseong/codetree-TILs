b038cadf4516de6801f455f9a5763e1f30ce518a
soyeong <korea020808@naver.com>
2024-09-25 12:48:44 +0900

    Title: 숫자의 증감, Time: 3832ms, Memory: 160MB, Status: Time Limit Exceed - CodeTree

--- 240925/숫자의 증감/increasing-and-decreasing-numbers.js
+++ 240925/숫자의 증감/increasing-and-decreasing-numbers.js
@@ -5,13 +5,14 @@ const c = input[0];
 const n = Number(input[1]);
 
 let result = "";
+let i, j;
 
 if (c === 'A') {
-    for (let i = 1; i <= n; i++) {
+    for (i = 1; i <= n; i++) {
         result += (i + " ");
     }
 } else {
-    for (let j = n; n >= 1; j--) {
+    for (j = n; n >= 1; j--) {
         result += (j + " ");
     }
 }
