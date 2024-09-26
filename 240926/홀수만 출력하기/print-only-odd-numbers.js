b7d2472195f0bb9ef3fdfb0441963e959a2b9a1f
soyeong <korea020808@naver.com>
2024-09-26 19:40:42 +0900

    Title: 홀수만 출력하기, Time: 191ms, Memory: 14MB, Status: Passed - CodeTree

--- 240926/홀수만 출력하기/print-only-odd-numbers.js
+++ 240926/홀수만 출력하기/print-only-odd-numbers.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split("\n") // 두 줄로 분리
-const N = Number(input[0]);
+const N = Number(input[0]); //첫 번쨰 줄에 주어지는 정수 N
 
-for (let i = 0; i <= N; i++) {
+for (let i = 1; i <= N; i++) { // \n으로 분리한 input 배열을 보면 0번 인덱스는 N에 해당하고, 1번부터 N번까지가 주어지는 정수에 해당한다.
     if (input[i] % 2 === 1 && input[i] % 3 === 0) {
         console.log(input[i]);
-    }
+     }
 }
