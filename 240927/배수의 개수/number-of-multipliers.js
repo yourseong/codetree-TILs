8638ee5ab39bd2c3d7b78f67f6878a98e544e411
soyeong <korea020808@naver.com>
2024-09-27 11:14:09 +0900

    Title: 배수의 개수, Time: 208ms, Memory: 16MB, Status: Passed - CodeTree

--- 240927/배수의 개수/number-of-multipliers.js
+++ 240927/배수의 개수/number-of-multipliers.js
@@ -4,9 +4,6 @@ const number = fs.readFileSync(0).toString().trim().split("\n");
 let cnt_3 = 0;
 let cnt_5 = 0;
 
-
-
-
 for (let i = 0; i <= 9; i++) {
     if (number[i] % 3 === 0) {
         cnt_3++;
