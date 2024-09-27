6afd089972cc0e9e018777911ef90a38c1707a3b
soyeong <korea020808@naver.com>
2024-09-27 12:12:16 +0900

    Title: 윤년 횟수 구하기, Time: 174ms, Memory: 20MB, Status: Wrong Answer - CodeTree

---
+++ 240927/윤년 횟수 구하기/number-of-leap-years.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const n = Number(fs.readFileSync(0).toString().trim());
+
+let cnt = 0;
+
+for (let i = 1; i < n; i++) {
+    if (i % 4 === 0 || (i % 100 !== 0 && i % 400 === 0)) {
+        cnt++;
+    }
+}
+
+console.log(cnt);
