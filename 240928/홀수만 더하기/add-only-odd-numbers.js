a32145c17af17850d18cc19ef953114a124e3fa9
soyeong <korea020808@naver.com>
2024-09-28 22:05:10 +0900

    Title: 홀수만 더하기, Time: 172ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240928/홀수만 더하기/add-only-odd-numbers.js
+++ 240928/홀수만 더하기/add-only-odd-numbers.js
@@ -9,10 +9,9 @@ const n = Number(input[0]);
 
 let sumVal = 0;
 
-for (let i = 1; i <= n; i++) {
+for (let i = 0; i <= n; i++) {
     if (input[i] % 2 === 1 && input[i] % 3 === 0) {
         sumVal += input[i];
-        console.log(sumVal);
     }
 }
 
