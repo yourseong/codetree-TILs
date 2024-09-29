3ebf3a4109d6b99c34dc6255f6c67ea6e7c55c9f
soyeong <korea020808@naver.com>
2024-09-29 19:31:47 +0900

    Title: a의 배수들의 곱, Time: 203ms, Memory: 17MB, Status: Passed - CodeTree

---
+++ 240929/a의 배수들의 곱/product-of-multiples-of-a.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split(" ");
+
+const a = Number(input[0]);
+const b = Number(input[1]);
+
+let prod = 1;
+
+for (let i = 1; i <= b; i++) {
+    if (i % a === 0) {
+        prod *= i;
+    }
+}
+
+console.log(prod);
