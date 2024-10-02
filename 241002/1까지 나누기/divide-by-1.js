193b79063122ce08731a3877d6b2a2a891156493
soyeong <korea020808@naver.com>
2024-10-02 13:20:27 +0900

    Title: 1까지 나누기, Time: 300ms, Memory: 14MB, Status: Passed - CodeTree

--- 241002/1까지 나누기/divide-by-1.js
+++ 241002/1까지 나누기/divide-by-1.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 let n = Number(fs.readFileSync(0).toString().trim());
 let cnt = 0;
 
-for (let i = 1; i <= n+1; i++) {
+for (let i = 1; i <= 100; i++) {
 
     n = parseInt(n / i);
     cnt++;
