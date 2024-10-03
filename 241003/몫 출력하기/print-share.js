4699a15a8ac903164ac7674314708248a571174a
soyeong <korea020808@naver.com>
2024-10-03 22:23:30 +0900

    Title: 몫 출력하기, Time: 183ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241003/몫 출력하기/print-share.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+while (true) {
+    let idx = 0;
+    idx++
+
+    
+    if (input[idx] % 2 === 0) {
+        console.log(input[idx] / 2);
+        break;
+        }
+    
+}
