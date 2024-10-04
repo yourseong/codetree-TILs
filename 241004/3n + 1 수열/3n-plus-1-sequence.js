3554986b92e7c27a0e3e2975694cfbb1248662e7
soyeong <korea020808@naver.com>
2024-10-04 13:19:45 +0900

    Title: 3n + 1 수열, Time: 205ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 241004/3n + 1 수열/3n-plus-1-sequence.js
+++ 241004/3n + 1 수열/3n-plus-1-sequence.js
@@ -4,17 +4,19 @@ let n = Number(fs.readFileSync(0).toString().trim());
 let cnt = 0;
 
 while (true) {
+    cnt++
     if (n % 2 === 0) {
         n /= 2;
-        cnt++
+        
     }
     else {
         n = n * 3 + 1;
-        cnt++
+        
     }
 
     if (n === 1) {
         console.log(cnt)
         break;
     }
+    
 }
