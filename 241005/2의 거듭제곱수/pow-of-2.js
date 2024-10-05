e6df3958aa084f25aa3623d8d74fb7726293f672
soyeong <korea020808@naver.com>
2024-10-05 22:36:04 +0900

    Title: 2의 거듭제곱수, Time: 5050ms, Memory: 17MB, Status: Time Limit Exceed - CodeTree

--- 241005/2의 거듭제곱수/pow-of-2.js
+++ 241005/2의 거듭제곱수/pow-of-2.js
@@ -1,13 +1,15 @@
 const fs = require("fs");
 let n = Number(fs.readFileSync(0).toString().trim());
+let cnt = 0;
 
 while (true) {
     n /= 2
+    cnt++
 
-    if(n / 2 === 1) 
+    if(n - 2 === 0) 
         break;
     
 }
 
 
-console.log(n);
+console.log(cnt + 1);
