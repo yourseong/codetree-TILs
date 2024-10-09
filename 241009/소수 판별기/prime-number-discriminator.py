9e004067218bda4b07f1daef1ab1919eca227902
soyeong <korea020808@naver.com>
2024-10-09 21:47:21 +0900

    Title: 소수 판별기, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241009/소수 판별기/prime-number-discriminator.py
+++ 241009/소수 판별기/prime-number-discriminator.py
@@ -3,10 +3,10 @@ n = int(input())
 state = 0
 
 for i in range(n + 1):
-    if (i % n == 0):
-        state = 1
+    if (i % n != 0):
+        state = 0
 
-if state == 1:
+if state == 0:
     print("P")
 else:
     print("C")
