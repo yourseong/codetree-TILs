f3d8dbc2890676a70cbc5d0b33bf9659f91a2515
soyeong <korea020808@naver.com>
2024-10-09 21:56:10 +0900

    Title: 소수 판별기, Time: 192ms, Memory: 14MB, Status: Passed - CodeTree

--- 241009/소수 판별기/prime-number-discriminator.js
+++ 241009/소수 판별기/prime-number-discriminator.js
@@ -4,7 +4,7 @@ const n = Number(fs.readFileSync(0).toString().trim());
 let state = 0;
 
 for (let i = 2; i <= n; i++) {
-    if (i != n && i % n === 0) { //i가 n이 아니면서 n을 나누어 떨어지게 만드는 경우
+    if (i != n && n % i === 0) { //i가 n이 아니면서 n을 나누어 떨어지게 만드는 경우
         state = 1; //소수가 아님 (소수가 아닌 경우를 1)
     }
 }
