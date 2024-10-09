9425d29bbf379b8c31a499971cb238750f3da670
soyeong <korea020808@naver.com>
2024-10-09 21:50:27 +0900

    Title: 소수 판별기, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241009/소수 판별기/prime-number-discriminator.py
+++ 241009/소수 판별기/prime-number-discriminator.py
@@ -3,10 +3,10 @@ n = int(input())
 state = 0
 
 for i in range(n + 1):
-    if (i % n != 0):
-        state = 0
+    if (i != 1 and i != n and i % n == 0): # 1과 자기 자신이 아닌 다른 수를 약수로 갖는 경우
+        state = 0 # 소수가 아니다
 
-if state == 0:
-    print("P")
+if state == 0: #소수이면
+    print("P") #P
 else:
     print("C")
