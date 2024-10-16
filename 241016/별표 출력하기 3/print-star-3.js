57208d7d9489cce67e3d813430e89ddc1f6debde
soyeong <korea020808@naver.com>
2024-10-16 23:19:36 +0900

    Title: 별표 출력하기 3, Time: 164ms, Memory: 14MB, Status: Passed - CodeTree

--- 241016/별표 출력하기 3/print-star-3.js
+++ 241016/별표 출력하기 3/print-star-3.js
@@ -5,15 +5,12 @@ let str = "";
 
 for (let i = 0; i < n; i++) { // 행 개수 (n개)
     str = "";
-
- for (let k = 0; k < i; k++) { // 공백 개수 
+    for (let k = 0; k < i; k++) { // 공백 개수 
         str += "  ";
     }
 
     for (let j = 0; j < 2*(n-i)- 1; j++) {
         str += "* ";
     }
-
-   
     console.log(str);
 }
