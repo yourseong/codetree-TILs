ec78895982f6fbad78e54f20141cb292f40a7258
soyeong <korea020808@naver.com>
2024-10-23 23:23:53 +0900

    Title: 별표 출력하기 9, Time: 246ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241023/별표 출력하기 9/print-star-9.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const n = Number(fs.readFileSync(0).toString().trim());
+
+let str = "";
+
+for (let i = 0; i < n; i++) { // 행 개수 (n개)
+    str = "";
+
+    for (let k = 0; k < n - i - 1; k++) { // 공백 개수 
+        str += "  ";
+    }
+    
+
+    for (let j = 0; j < 2 * i + 1; j++) {
+        str += "* ";
+    }
+
+    
+    console.log(str);
+}
