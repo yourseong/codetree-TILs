3ec346015204207e8c3ae34e53cb40a2cad258a6
Yuseong Chae <korea020808@naver.com>
2025-05-01 19:54:55 +0900

    Title: 1-9 정사각형, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250501/1-9 정사각형/1-9-square.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+cnt = 0
+
+for i in range(n):
+    for j in range(n):
+        cnt+=1
+        if cnt <= 9:
+            print(cnt, end="")
+        if cnt >= 10:
+            cnt = 1
+            print(cnt, end="")
+    print()
+
+        
