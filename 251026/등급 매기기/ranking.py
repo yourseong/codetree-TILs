677a359aa036263151bc5f722433ce24031ff91b
Yuseong Chae <korea020808@naver.com>
2025-10-26 20:00:39 +0900

    Title: 등급 매기기, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251026/등급 매기기/ranking.py
@@ -0,0 +1,15 @@
+n = int(input())
+grade = ""
+
+if n >= 90:
+    grade = "A"
+elif n >= 80:
+    grade = "B"
+elif n >= 70:
+    grade = "C"
+elif n >= 60:
+    grade = "D"
+else:
+    grade = "F"
+
+print(grade)
