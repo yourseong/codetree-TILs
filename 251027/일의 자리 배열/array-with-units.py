75f331314a8e77cd6faefbc28f3f6d60c5fb3ab4
Yuseong Chae <korea020808@naver.com>
2025-10-27 04:07:31 +0900

    Title: 일의 자리 배열, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251027/일의 자리 배열/array-with-units.py
@@ -0,0 +1,16 @@
+a, b = map(int, input().split())
+
+# 2, 5가 입력되면 2, 5가 첫번째, 두번째 항으로 설정됨
+arr = [a, b]
+arr.append(a+b) # 세 번째 항까지 설정
+
+# a + b 한다음 3번째 항부터는 이렇게
+# 2 + 5 = 7 (3번째 항) : 리스트 상태 - 2 5 7
+# 7 + 5 = 12 (4번째 항 = 2) : 리스트 상태 - 2, 5, 7, 2
+print(a, b, end=" ")
+for i in range(2, 10):
+    next_elem = (arr[-1] + arr[-2]) % 10
+    arr.append(next_elem)
+    print(arr[i], end=" ")
+
+
