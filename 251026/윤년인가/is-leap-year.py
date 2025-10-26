a5eba6aa03c5e3de32f0c6b62d05da10adca4496
Yuseong Chae <korea020808@naver.com>
2025-10-26 22:14:39 +0900

    Title: 윤년인가, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251026/윤년인가/is-leap-year.py
+++ 251026/윤년인가/is-leap-year.py
@@ -1,8 +1,13 @@
 y = int(input())
 
-if (y % 4 == 0):
+# 200 : false 나와야 함 - 100으로 나누어 떨어지는데 400으로 나누어 떨어지지 않는 수
+
+# 100으로 나누어 떨어지되 400으로 나누어 떨어지지 않는 해는 평년
+if (y % 100 == 0 and y % 400 != 0):
+    print("false")
+# 4로 나누어떨어지면 true (윤년)
+elif (y % 4 == 0):
     print("true")
-elif (y % 100 == 0 and y % 400 == 1):
-    print("true")
+# 그 밖의 해는 평년 (false)
 else:
     print("false")
