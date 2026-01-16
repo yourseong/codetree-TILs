2228affba99c987660f6d2795ceaad284f47e020
Yuseong Chae <spitz910325@naver.com>
2026-01-16 14:41:12 +0900

    Title: 학점 계산기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260116/학점 계산기/credit-calculator.py
@@ -0,0 +1,23 @@
+# 과목 수 n
+# n개 과목 학점 입력받아서 평균학점 구해 출력
+# 4.0 >= Perfect, 3.0>= Good, else == Poor
+
+n = int(input())
+grades = list(map(float, input().split()))
+
+aveg = 0
+total = 0
+
+for i in range(n):
+    total += grades[i]
+
+aveg = total / n
+print(f"{aveg:.1f}")
+
+if aveg > 4.0:
+    print("Perfect")
+elif aveg > 3.0:
+    print("Good")
+else:
+    print("Poor")
+
