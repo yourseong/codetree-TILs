f8e992368b99defe1e137ba542f1e70d7e99226f
Yuseong Chae <spitz910325@naver.com>
2026-01-16 16:13:17 +0900

    Title: 배열의 특정 위치 찾기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260116/배열의 특정 위치 찾기/find-specific-location-fo-array.py
+++ 260116/배열의 특정 위치 찾기/find-specific-location-fo-array.py
@@ -2,6 +2,16 @@ ints = list(map(int, input().split()))
 
 # 짝수 번째 입력된 값의 합
 
-3_total = sum(ints[::3])
+even_total = 0
+three_total = 0
 
-print(even_total)
+
+for i in range(len(ints)):
+    if (i + 1) % 2 == 0:
+        even_total += ints[i]
+    if (i + 1) % 3 == 0:
+        three_total += ints[i]
+
+aveg = round(three_total / 3, 1)
+
+print(even_total, aveg)
