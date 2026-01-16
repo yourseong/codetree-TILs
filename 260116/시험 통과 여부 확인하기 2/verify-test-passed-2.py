a643ec4c8be5be269b41fa68a75e7d7d82e52e02
Yuseong Chae <spitz910325@naver.com>
2026-01-16 15:50:02 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260116/시험 통과 여부 확인하기 2/verify-test-passed-2.py
+++ 260116/시험 통과 여부 확인하기 2/verify-test-passed-2.py
@@ -1,5 +1,27 @@
 # 학생수
 n = int(input())
 
-std1 = list(map(int, input().split()))
-std1 = list(map(int, input().split()))
+total = 0
+aveg = 0
+cnt = 0
+
+# 학생수만큼 시험점수 배열 입력받기
+# i == 학생수
+for i in range(n):
+    grades = list(map(int, input().split()))
+
+    # j = 힉생별 점수 인덱스
+    for j in range(4):
+        total += grades[j]
+    aveg = total / 4
+    total = 0
+
+    if (aveg >= 60):
+        print("pass")
+        cnt += 1
+    else:
+        print("fail")
+
+print(cnt)
+    
+
