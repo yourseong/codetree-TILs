7e5b6d56c3c02d629a2c7b17255b6c99724d7d81
Yuseong Chae <spitz910325@naver.com>
2026-01-23 16:51:12 +0900

    Title: 나눗셈의 나머지, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260123/나눗셈의 나머지/remainder-of-division.py
+++ 260123/나눗셈의 나머지/remainder-of-division.py
@@ -10,17 +10,17 @@ cnt = 0
 cnt_list = [0] * 10 # 나머지 세는거
 mod_list = [] # 나머지들 저장
 
-while(a > 1): # A가 1보다 작아지면 종료
-    mod = a % b
+while(a > 1): # A가 1보다 작아지면 종료 (등호로 표시하면 1보다 작을때도 포함하므로 부등호로 해야함)
+    mod = a % b # 나머지 구하기
     # print(mod)
-    a //= b
+    a //= b # 나눈 결과 저장
     # print(a)
-    mod_list.append(mod)
+    mod_list.append(mod) # 나머지 리스트에 나머지들 추가
 
 # print(mod_list)
 
-for elem in mod_list:
-    cnt_list[elem] += 1
+for elem in mod_list: # 나마지 리스트에서 
+    cnt_list[elem] += 1 # 등장 횟수 세기
 
-new_arr = [num**2 for num in cnt_list]
-print(sum(new_arr))
+new_arr = [num**2 for num in cnt_list] # 각 요소들 제곱 후
+print(sum(new_arr)) # 더한 결과 저장
