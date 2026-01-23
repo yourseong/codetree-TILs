2a5c6b1e6f135d46688323e6e7f5619452509321
Yuseong Chae <spitz910325@naver.com>
2026-01-23 13:52:41 +0900

    Title: 십의 자리 숫자, Time: 77ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 260123/십의 자리 숫자/number-of-tens-digit.py
+++ 260123/십의 자리 숫자/number-of-tens-digit.py
@@ -1,7 +1,7 @@
 # 0이 주어지면 그 0을 제와하고 그 떄까지 입력된 정수의 숫자들의
 # 십의 자리 숫자가 각각 몇 개인지 작은 수부터 출력
 
-count_list = [0] * 100
+count_list = [0] * 99
 arr = list(map(int, input().split()))
 
 for i in range(len(arr)):
