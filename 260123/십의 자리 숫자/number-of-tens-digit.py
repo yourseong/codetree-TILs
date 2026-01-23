284f7754d5fb09c9ba4da801afd971ea42d27dc9
Yuseong Chae <spitz910325@naver.com>
2026-01-23 14:13:08 +0900

    Title: 십의 자리 숫자, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260123/십의 자리 숫자/number-of-tens-digit.py
+++ 260123/십의 자리 숫자/number-of-tens-digit.py
@@ -5,10 +5,10 @@ count_list = [0] * 10 # 십의자리 숫자sms 1 ~ 9까지 이므로 10까지만
 arr = list(map(int, input().split())) # 입력받는 숫자들
 
 for elem in arr: # arr 순회해서
-    if elem == 0:
-        break
-    elem //= 10 # 몫만 저장
-    count_list[elem] += 1
+    if elem == 0: # 0 이면 
+        break # 종료 (0 미포함하기위함)
+    elem //= 10 # 몫만 저장 
+    count_list[elem] += 1 # 카운트 +1
 
 for i in range(1, 10):
     print(f"{i} - {count_list[i]}")
