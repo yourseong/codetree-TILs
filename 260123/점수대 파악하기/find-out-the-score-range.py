cbf65afbb0d8d71519cce6eff0f19b8645b6d5dd
Yuseong Chae <spitz910325@naver.com>
2026-01-23 14:29:05 +0900

    Title: 점수대 파악하기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260123/점수대 파악하기/find-out-the-score-range.py
+++ 260123/점수대 파악하기/find-out-the-score-range.py
@@ -6,7 +6,7 @@ count_list = [0] * 11
 for elem in arr:
     if elem == 0: # 0 주어지면
         break # 종료
-    if elem > 10: # 10점 이상인 학생만 카운트
+    if elem >= 10: # 10점 이상인 학생만 카운트
         elem //= 10
         count_list[elem] += 1
 
