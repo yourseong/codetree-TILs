8a31dba81bbbf8feacaa4256901d30e6876e8969
Yuseong Chae <spitz910325@naver.com>
2026-03-14 00:09:32 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260314/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -0,0 +1,38 @@
+# n년간의 자동차 가격 정보 : 예를 들어 5면
+n = int(input())
+
+# 9 10 2 3 6인 경우
+price = list(map(int, input().split()))
+
+# 출력 : 4
+
+# 자동차를 단 한 번 사서 되팔 때의 이익 최대화하기
+# 자동차를 사기 전에는 팔 수 없음
+
+# 사는 시점은 언제든 될 수 있다
+# 싼 가격 계속 업데이트
+
+# 언제 사도 이득이 안되는 경우 : 숫자가 내림차순으로 주어질 때
+# 최솟값의 인덱스가 마지막 인덱스가 아니면 무조건 이익 발셍
+
+buy_value = price[0] # 구매가 초기화
+profit = 0 # 현재 이익
+max_profit = 0 # 최대 이익
+
+if min(price) != price[n-1]:
+    # buy_value = min(price) # !수정사항! 전체 최솟값에서 사는것이 꼭 이득이 아닐수도 있음
+
+    # 반복문 돌면서 최대 이익 출력
+    for i in range(n): 
+        profit = buy_value - price[i]
+        if max_profit > profit:
+            max_profit = profit
+        if buy_value > price[i]:
+            buy_value = price[i]
+    print(abs(max_profit))
+
+else:
+    print(0)
+
+
+
