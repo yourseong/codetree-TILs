ca27b589cf1852cdb97f37687ce16cdac35cb22a
Yuseong Chae <spitz910325@naver.com>
2026-03-13 20:09:36 +0900

    Title: 두 수의 차의 최솟값, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260313/두 수의 차의 최솟값/minimum-difference-between-two-numbers.py
@@ -0,0 +1,21 @@
+n = int(input())
+nums = list(map(int, input().split())) # n개의 정수가 오름차순으로 주어짐
+
+# nums중 서로 다른 두 개의 정수를 골랐을 때
+# 두 정수의 차가 최소가 되는 경우, 그 차이를 출력
+
+# 현재 수 - 다음 수
+# 결과가 작은 값 업데이트
+# 오름차순 배열에서는 항상 인접한 수 차이가 제일 작음
+
+min_diff = 9999999
+
+for i in range(n-1):
+    diff = nums[i+1] - nums[i] # 인접한 숫자끼리 빼는데
+    if min_diff > diff: # 더 작은 값이 나타난다면
+        min_diff = diff
+
+print(min_diff)
+    
+
+    
