08862374195a6ae04276629cd25d2fd7cca345c4
Yuseong Chae <spitz910325@naver.com>
2026-03-17 17:22:59 +0900

    Title: 배열의 평균, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260317/배열의 평균/ave-of-array.py
@@ -0,0 +1,20 @@
+# 2행 4열 배열
+
+sum_val = 0
+
+horizon_list = [] # 가로 값을 더해 넣을 리스트
+arr_2d = [] # 2차원 배열 담기
+
+for i in range(2): #2행
+    arr_1d = list(map(int, input().split())) # 첫 번째 열을 먼저 받고
+    arr_2d.append(arr_1d)
+
+print(sum(arr_2d[0])/4, sum(arr_2d[1])/4) # 가로 평균
+
+# 세로 평균
+# arr_2d 순회하면서 
+for j in range(4):
+    vertical_aveg = (arr_2d[0][j] + arr_2d[1][j])/2
+    print(vertical_aveg, end = " ")
+print("")
+print((sum(arr_2d[0]) + sum(arr_2d[1]))/8) # 전체 평균
