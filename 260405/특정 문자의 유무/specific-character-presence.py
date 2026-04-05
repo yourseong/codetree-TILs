bcf11e906e6b661443992b596e9888ed5923c11f
Yuseong Chae <spitz910325@naver.com>
2026-04-05 16:29:47 +0900

    Title: 특정 문자의 유무, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260405/특정 문자의 유무/specific-character-presence.py
@@ -0,0 +1,12 @@
+str = input().lower()
+
+ee_exists = "No"
+ab_exists = "No"
+
+# ee가 존재하는가
+if 'ee' in str:
+    ee_exists = "Yes"
+if 'ab' in str:
+    ab_exists = "Yes"
+    
+print(ee_exists, ab_exists)
