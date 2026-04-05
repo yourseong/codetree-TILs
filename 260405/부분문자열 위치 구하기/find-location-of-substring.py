ad9da98ce64f8b67d9693cc105374372ee015e2d
Yuseong Chae <spitz910325@naver.com>
2026-04-05 18:07:20 +0900

    Title: 부분문자열 위치 구하기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260405/부분문자열 위치 구하기/find-location-of-substring.py
@@ -0,0 +1,13 @@
+input_str = input()
+target_str = input()
+
+# 목적 문자열이 부분 문자열로 존재하는 경우, 부분 문자열의 시작 인덱스 출력
+
+# input_str 안에 target_str이 있으면 target의 시작 인덱스 출력
+
+for i in range(len(input_str) - 1):
+    if input_str[i:i+2] == target_str:
+        print(i)
+        break
+if target_str not in input_str:
+    print(-1)
